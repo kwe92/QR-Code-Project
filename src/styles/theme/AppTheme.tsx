6a244e1de555faf1b220c0ffc5ec4f4fe581d7a6
Kweayon  Clark <Kweayon@gmail.com>
2023-01-26 20:14:02 -0600

    App Theme Object

---
+++ src/styles/theme/AppTheme.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ThemeInterface } from "../../interfaces/theme/ThemeInterface";
+
+const AppTheme: ThemeInterface = {
+    whiteColor: "hsl(0, 0%, 100%)",
+    lightGreyColor: "hsl(212, 45%, 89%)",
+    greyishBlueColor: "hsl(220, 15%, 55%)",
+    darkBlueColor: "hsl(218, 44%, 22%)",
+    fontFamily: "Outfit",
+    bodyFont: "0.9375rem",
+    lightBold: 400,
+    heavyBold: 700,
+    borderRadius: "0.75rem;"
+};
+
+export default AppTheme;
