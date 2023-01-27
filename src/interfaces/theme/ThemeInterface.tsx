cd54bcaad7862c8090a59f954f8e2c01de0b36b0
Kweayon  Clark <Kweayon@gmail.com>
2023-01-26 20:13:23 -0600

    Theme Interface

---
+++ src/interfaces/theme/ThemeInterface.tsx
@@ -0,0 +1,12 @@
+export interface ThemeInterface {
+    whiteColor: string,
+    lightGreyColor: string,
+    greyishBlueColor: string,
+    darkBlueColor: string,
+    fontFamily: string,
+    bodyFont: string,
+    lightBold: number,
+    heavyBold: number,
+    borderRadius: string,
+}
+
